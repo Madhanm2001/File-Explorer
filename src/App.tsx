24a77228174e95ef31d08f2d0ff509b596d55e9c
Madhan <mmadhankumar519@gmail.com>
2025-08-05 12:45:35 +0530

    file explorer repository

---
+++ src/App.tsx
@@ -0,0 +1,321 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Folder from './component/folder';
+import File from './component/file';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  let objectList: any = {
+    public: {
+      files: [{ name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+      folderId: uuidv4(),
+      isToggle: false,
+      isclose: false,
+      first: {
+        files: [{ name: 'first.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+        folderId: uuidv4(),
+        five: {
+          files: [{ name: 'first.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+          folderId: uuidv4(),
+          isToggle: false,
+          isclose: true,
+        },
+        isToggle: false,
+        isclose: true,
+      }
+    },
+    rrr: {
+      files: [{ name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+      folderId: uuidv4(),
+      isToggle: false,
+      isclose: false,
+      first: {
+        files: [{ name: 'first.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+        folderId: uuidv4(),
+        five: {
+          files: [{ name: 'first.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],
+          folderId: uuidv4(),
+          isToggle: false,
+          isclose: true,
+        },
+        isToggle: false,
+        isclose: true,
+      }
+    },
+    src: {
+      files: [{ name: 'index.js', id: uuidv4() }, { name: 'index.js', id: uuidv4() }],
+      folderId: uuidv4(),
+      inner: {
+        files: [{ name: 'index.css', id: uuidv4() }, { name: 'erer.css', id: uuidv4() }, { name: 'sas.css', id: uuidv4() }],
+        folderId: uuidv4(),
+        isToggle: false,
+        isclose: true,
+        hhhhfhgf: {
+          files: [{ name: 'index.css', id: uuidv4() }, { name: 'erer.css', id: uuidv4() }, { name: 'sas.css', id: uuidv4() }],
+          folderId: uuidv4(),
+          isToggle: false,
+          isclose: true,
+        }
+      },
+      isToggle: false,
+      isclose: false,
+    },
+    source: {
+      files: [{ name: 'index.js', id: uuidv4() }, { name: 'index.js', id: uuidv4() }],
+      folderId: uuidv4(),
+      outer: {
+        files: [{ name: 'index.css', id: uuidv4() }, { name: '77777.css', id: uuidv4() }, { name: 'sas.css', id: uuidv4() }],
+        folderId: uuidv4(),
+        isToggle: false,
+        isclose: true,
+        hhhh: {
+          files: [{ name: 'index.css', id: uuidv4() }, { name: 'erer.css', id: uuidv4() }, { name: 'sas.css', id: uuidv4() }],
+          folderId: uuidv4(),
+          isToggle: false,
+          isclose: true,
+        }
+      },
+      isToggle: false,
+      isclose: false,
+    },
+    file: { fileId: uuidv4(), files: [{ name: 'package.json', id: uuidv4() }] },
+  }
+  const [objList, setObjList]: any = useState(objectList)
+
+  const [arrayObj, setArrayObj] = useState<any[]>([])
+
+
+  useEffect(() => {
+ console.log("updated");
+ 
+    onArrayObj()
+
+  }, [objList])
+
+  const onArrayObj = () => {
+    console.log("trigger",objList);
+    
+    //  setArrayObj([])
+    const temp: any = [];
+    for (let key in objList) {
+      temp.push({ folderName: key, folder: objList[key] })
+    }
+    setArrayObj(temp)
+    console.log("checking", arrayObj)
+  }
+
+  const updateFunction = (method: any, folderId?: any, isToggle?: any, fileId?: any, folderName?: any, fileName?: any) => {
+
+    let updated: any
+    if (method === 'toggleFolder') {
+      updated = flattenToogleFolder(objList, folderId, isToggle);
+
+    }
+    else if (method === 'addFolder') {
+      updated = flattenAddFolder(objList, folderId, folderName);
+
+    }
+    else if (method === 'deleteFolder') {
+      updated = flattenDeleteFolder(objList, folderId);
+
+    }
+    else if (method === 'addFile') {
+      updated = flattenAddFile(objList, folderId, fileName);
+
+    }
+    else if (method === 'deleteFile') {
+      updated = flattenDeleteFile(objList, folderId, fileId);
+
+    }
+
+    setObjList(updated);
+    console.log(objList, "✅ updated objectList");
+
+  }
+
+
+
+  const flattenToogleFolder = (objectList: any, id: any, isToggle: any) => {
+    const copy = Array.isArray(objectList) ? [...objectList] : { ...objectList };
+
+    for (let key in copy) {
+
+      if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key])) {
+
+        if (copy[key].folderId == id) {
+
+          let status = isToggle
+          const onToggleFolder = (copy: any) => {
+            for (let key in copy) {
+              if (typeof (copy[key]) == 'object') {
+                console.log("key", key, copy.folderId != id)
+                if (copy.folderId != id) {
+                  console.log("yes Id")
+                  copy.isclose = status
+                  copy.isToggle = !status
+                }
+                else {
+                  copy.isToggle = !status
+                }
+                onToggleFolder(copy[key])
+              }
+            }
+          }
+
+          onToggleFolder(copy[key])
+
+
+
+        }
+
+        if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key]))
+          flattenToogleFolder(copy[key], id, isToggle)
+
+      }
+
+    }
+    return copy
+  }
+
+  const flattenAddFolder = (objectList: any, id: any, name: string) => {
+    
+    const copy = Array.isArray(objectList) ? [...objectList] : { ...objectList };
+
+    for (let key in copy) {
+
+      if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key])) {
+
+        if (copy[key].folderId == id) {
+
+          copy[key][name]={folderId:uuidv4(),files: [{ name: 'first.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }, { name: 'index.html', id: uuidv4() }],isToggle:false,isclose:!copy[key].isToggle}
+          break;
+
+
+
+        }
+
+        if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key]))
+          flattenAddFolder(copy[key], id,name)
+
+      }
+
+    }
+    return copy
+
+  }
+  const flattenDeleteFolder = (objectList: any, id: any) => {
+
+    const copy = Array.isArray(objectList) ? [...objectList] : { ...objectList };
+
+    for (let key in copy) {
+
+      if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key])) {
+
+        if (copy[key].folderId == id) {
+
+          const onToggleFolder = (copy: any) => {
+            for (let key in copy) {
+              if (typeof (copy[key]) == 'object') {
+                console.log("key", key, copy.folderId != id)
+                if (copy.folderId == id) {
+                   
+                   delete copy[key]
+                  
+                }
+
+                onToggleFolder(copy[key])
+              }
+            }
+          }
+
+          onToggleFolder(copy[key])
+
+
+
+        }
+
+        if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key]))
+          flattenDeleteFolder(copy[key], id)
+
+      }
+
+    }
+    return copy
+
+  }
+
+  const flattenAddFile = (objectList: any, id: any, fileName: any) => {
+
+    const copy = Array.isArray(objectList) ? [...objectList] : { ...objectList };
+
+    for (let key in copy) {
+
+      if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key])) {
+
+        if (copy[key].folderId == id) {
+
+          copy[key].files.push({ name: fileName, id: uuidv4() })
+
+        }
+
+        if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key]))
+          flattenAddFile(copy[key], id, fileName)
+      }
+
+    }
+
+    return copy
+
+  }
+
+  const flattenDeleteFile = (objectList: any, id: any,fileId:any) => {
+
+    const copy = Array.isArray(objectList) ? [...objectList] : { ...objectList };
+
+    for (let key in copy) {
+
+      if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key])) {
+
+        if (copy[key].folderId == id) {
+
+          const temp= copy[key].files.filter((data:any)=>{
+                     return(data.id!=fileId)
+             })
+
+            copy[key].files=temp
+
+        }
+
+        if (typeof (copy[key]) == 'object' && !Array.isArray(copy[key]))
+          flattenDeleteFile(copy[key], id,fileId)
+      }
+
+    }
+
+    return copy
+
+  }
+
+  console.log(arrayObj, 'arrayObj');
+
+  return (
+    <div style={{ padding: '0 10px' }}>
+      <h2 style={{ textAlign: 'center', color: 'white' }}>File Explorer</h2>
+      {arrayObj && arrayObj.map((data, id) => {
+        console.log("count", id, arrayObj.length)
+        const currObj = { [data.folderName]: data.folder }
+        console.log("currrret", currObj);
+
+        return (
+          <>
+            <Folder key={id} folderObj={currObj} updateFunction={updateFunction} />
+          </>
+        )
+
+      })}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/folder.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react'
+import File from './file'
+import '../App.css'
+// Folder collapsed icon
+import { MdExpandMore } from "react-icons/md";
+
+// Folder expanded icon
+import { MdExpandLess } from "react-icons/md";
+
+// Delete file/folder icon
+import { MdDeleteOutline } from "react-icons/md";
+
+// Add new folder icon
+import { FiFolderPlus } from "react-icons/fi";
+
+// Add new file icon
+import { AiOutlineFileAdd } from "react-icons/ai";
+
+
+const Folder = ({ folderObj ,updateFunction,onDeleteFile}: any) => {
+    console.log(folderObj,"kkkkkkkkkkkkk")
+    // const [files, setFiles] = useState<any[]>([])
+    const [folderArray, setFolderArray] = useState<any[]>([])
+    // const[folderObject,setFolderObj]=useState(folderObj)
+    console.log(folderObj,"folderObject");
+    
+
+useEffect(()=>{
+console.log(folderArray,"currenrObj");
+    flatten(folderObj)
+},[folderObj])
+let n=1
+const temp: any = []
+const  flatten=(folderObj:any)=>{
+    let folder:any|undefined
+    
+    for(let key in folderObj){
+        if(typeof(folderObj[key])=='object'){
+         if(folderObj[key]){
+            if(key === "public") console.log("key mama", folderObj[key].isToggle)
+            folder={folder:key,files:folderObj[key].files,folderId:folderObj[key].folderId,isToggle:folderObj[key].isToggle,isclose:folderObj[key].isclose,marginLeft:n,fileId:folderObj[key].fileId}
+        }
+        if(folder.files!=undefined && (folder.folderId==undefined||folder.fileId==undefined)){
+            temp.push(folder)
+            n+=25
+            flatten(folderObj[key])
+        }
+    }
+    
+}
+console.log("temp", temp)
+setFolderArray(temp)
+}
+
+    return (
+        <div>
+         {folderArray.length > 0 && folderArray.map((folder,id)=>{
+            console.log(folder,"folder mama");
+            
+           return(
+           <li key={id} style={{listStyle:'none',marginTop:'10px'}}>
+            {!folder.fileId&&(!folder.isclose?<div style={{display:'flex',gap:'10px',marginLeft:`${folder.marginLeft}px`}}>
+                <div style={{display:'flex',flexWrap:'wrap',gap:'10px'}} id='folder'>
+                    <div onClick={()=>{updateFunction('toggleFolder',folder.folderId,folder.isToggle)}} style={{display:'flex',gap:'5px',cursor:'pointer'}}>
+                    <p style={{marginTop:'18px'}}>{folder.isToggle?<MdExpandLess style={{color:'white'}} size={18} />:<MdExpandMore style={{color:'white'}} size={18} />}</p>
+                    <h1 style={{color:'white',fontSize:'17px',fontFamily:'sans-serif',marginTop:'17px',wordBreak:'break-word'}}>{folder.folder}</h1>
+                    </div>
+                <div style={{display:'flex',marginTop:'18px',gap:'10px'}}><AiOutlineFileAdd onClick={()=>updateFunction('addFile',folder.folderId,"","","","Ben 10")} style={{color:'yellow',cursor:'pointer'}} size={18}/><FiFolderPlus onClick={()=>{updateFunction('addFolder',folder.folderId,"","","mango","")}} style={{color:'#0CBA00',cursor:'pointer'}} size={18}/><MdDeleteOutline onClick={()=>{updateFunction('deleteFolder',folder.folderId)}}  style={{color:'red',cursor:'pointer'}} size={18}/></div></div>
+                </div>:'')}
+                 {/* {folder.isToggle&&<hr style={{color:'grey',width:'auto'}}/>} */}
+            {((folder.isToggle && !folder.isclose) || folder.fileId) &&
+            <li style={{marginLeft:`${folder.fileId?0:folder.marginLeft}px`}}>
+                <File folder={folder} updateFunction={updateFunction}/>
+            </li>}
+           </li> 
+        )
+         })} 
+        </div>
+    )
+}
+
+export default Folder
